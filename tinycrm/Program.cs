a19934a1e871f35731ad4713fe07df2874db1980
Dimitris Petrogiannos <jim-petr47@hotmail.gr>
2020-04-30 16:17:46 +0300

    Add project files.

---
+++ tinycrm/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace tinycrm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
